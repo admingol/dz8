6ca919801f0d82eeb9ad3a9148784f7ae50b55d0
тима <timofijakin@gmail.com>
2023-11-20 14:37:54 +0200

    dz8

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import time
 import logging
 
-# Налаштування логування
 logging.basicConfig(filename='timing.log', level=logging.INFO)
 
 def timing(func, *args, **kwargs):
@@ -10,7 +9,6 @@ def timing(func, *args, **kwargs):
     end_time = time.time()
     execution_time = end_time - start_time
 
-    # Запис інформації у журнал
     logging.info(f"Функція '{func.__name__}' виконана за {execution_time} секунд")
 
     return result, execution_time
@@ -18,10 +16,9 @@ def timing(func, *args, **kwargs):
 def example_function():
     time.sleep(2)
 
-# Виклик функції timing для вимірювання часу виконання example_function
+
 result, execution_time = timing(example_function)
 
-# Виведення результатів на консоль
 print(f"Результат: {result}")
 print(f"Час виконання: {execution_time} секунд")
 
